7eef68431a5c791fb78eabe5fca96315d1a7336e
lucasdcs <luucas_dc@hotmail.com>
2024-06-19 20:19:57 -0300

    Tela de login realizada sem widgets

--- app/lib/ui/pages/login_page.dart
+++ app/lib/ui/pages/login_page.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:app_flutter/ui/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -21,34 +26,92 @@ class _LoginPageState extends State<LoginPage> {
         key: _formKey,
         child: Center(
         child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextFormField(
-                controller: _cpfController,
-                keyboardType: TextInputType.number,
-                validator: (cpf) {
-                  if(cpf == null || cpf.isEmpty){
-                    return 'Por favor, digite seu cpf';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                validator: (senha) {
-                  if(senha == null || senha.isEmpty) {
-                    return 'Por favor, digite sua senha';
-                  } else if(senha.length < 6) {
-                    return 'Por favor, digite uma senha maior que 6 caracteres';
-                  }
-                  return null;
-                },
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 20.0),
+                child:
+                TextFormField(
 
+                  decoration: const InputDecoration(
+                    labelText: 'CPF',
+                    hintText: '000.000.000-00',
+                    border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blueAccent), // Borda ao focar
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey), // Borda ao habilitar
+                    ),
+                  ),
+
+                  controller: _cpfController,
+                  keyboardType: TextInputType.text,
+                  validator: (cpf) {
+                    if(cpf == null || cpf.isEmpty){
+                      return 'Preencha seu CPF!';
+                    }
+                    return null;
+                  },
+                ),
               ),
-              ElevatedButton(onPressed: (){}, child: Text('ENTRAR'),),
+
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'SENHA',
+                hintText: 'Digite sua senha',
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueAccent), // Borda ao focar
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey), // Borda ao habilitar
+                ),
+              ),
+
+              controller: _passwordController,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              validator: (senha) {
+                if(senha == null || senha.isEmpty){
+                  return 'Preenche sua senha!';
+                } else if(senha.length < 6) {
+                  return 'Por favor digite uma senha maior que 6 caracteres';
+                }
+                return null;
+              },
+            ),
+              const SizedBox(height: 30),
+              Container(
+                margin: const EdgeInsets.only(top: 50.0), // Adiciona margem superior ao botão
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+                    if(_formKey.currentState!.validate()){
+                      bool deuCerto = await login();
+                    if(!currentFocus.hasPrimaryFocus) {
+                      currentFocus.unfocus();
+                    }
+                    if(deuCerto) {
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage(),
+                        ),
+                      );
+                    } else {
+                      _passwordController.clear();
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                    ),
+                    child: const Text('ENTRAR'),
+
+                ),
+              ),
             ],
           ),
         ),
@@ -56,9 +119,31 @@ class _LoginPageState extends State<LoginPage> {
     ),
     );
   }
+  
+  final snackBar = const SnackBar(
+      content: Text(
+        'cpf ou senha são invaliidos',
+        textAlign: TextAlign.center,
+      ),
+        backgroundColor: Colors.redAccent,
+  );
 
   Future<bool> login() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     var url = Uri.parse('https://minhasapis.com.br/login/');
+    var resposta = await http.post(
+      url,
+    body: {
+      'username': _cpfController.text,
+      'password': _passwordController.text
+    },
+    );
+
+    if(resposta.statusCode == 200){
+      await sharedPreferences.setString('token', "Token${jsonDecode(resposta.body)['token']}");
+      return true;
+    } else {
+      return false;
+    }
   }
 }
