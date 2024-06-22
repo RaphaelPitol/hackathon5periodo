252d4ac6a16228696a7dd74ef675e5c30b421c67
alexandreheigi82 <administrador.eig@gmail.com>
2024-06-22 08:14:36 -0300

    cadastro de novo usuario, salvar no bd, rotas para vacinas e historico medico

---
+++ app/lib/ui/pages/vacinas_pendentes_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VacinasPendentesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vacinas Pendentes'),
+      ),
+      body: Center(
+        child: Text('Conteúdo das Vacinas Pendentes'),
+      ),
+    );
+  }
+}
